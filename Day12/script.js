2058df9c4d64611873a3b13f0015ad910587da04
arshpull <arshadahmed.hash@gmail.com>
2025-09-17 19:54:17 +0530

    DOM creation

---
+++ Day12/script.js
@@ -0,0 +1,32 @@
+const taskInput = document.getElementById("taskInput");
+const mainDiv = document.getElementById("main");
+let todos = [];
+function addTask() {
+  todos.push({
+    task: taskInput.value,
+  });
+  displayData();
+}
+function displayData() {
+  mainDiv.textContent = "";
+  todos.forEach((t,ind) => {
+    const div = document.createElement("div");
+    div.classList.add("task");
+    const taskName = document.createElement("p");
+    taskName.textContent = t.task;
+    const button = document.createElement("button");
+    button.textContent = "❌";
+    button.onclick=function(){
+        const res= confirm  ("Do you want to delete?")
+        if(res)
+        {
+            todos.splice(ind,1);
+            alert("😂,you deleted a wish")
+            displayData();
+        }
+    }
+    div.append(taskName, button);
+    mainDiv.appendChild(div);
+    taskInput.value=''
+  });
+}
